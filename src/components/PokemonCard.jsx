fb5ecd1ee02954520c52899da43d203c03e6696e
Antoine <ant.delalande@gmail.com>
2024-03-25 17:34:24 +0100

    added pokemon list and variables

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,11 @@
 const card = {
     padding: "2em",
-    backgroundColor: "gray",
+    backgroundColor: "silver",
     border: "2px solid black",
 }
 
 const cardImg = {
-    boxShadow: "0px 0px 5px gray",
+    boxShadow: "0px 0px 5px silver",
     backgroundColor: "white",
     border: "2px solid black",
 }
@@ -17,10 +17,23 @@ const cardText = {
     border: "2px solid black",
 }
 
+const pokemonList = [
+    {
+        name: "Bulbizarre",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    },
+    {
+        name: "Mew"
+    }
+]
+
+const pokemon = pokemonList[0]
+
+
 function PokemonCard() {
     return <figure style={card}>
-        <img style={cardImg} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizarre" />
-        <figcaption style={cardText}>Bulbizarre</figcaption>
+        {pokemon.imgSrc ? <img style={cardImg} src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+        <figcaption style={cardText}>{pokemon.name }</figcaption>
     </figure>
 }
 
