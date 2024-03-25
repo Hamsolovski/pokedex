2e3a4215296d2df070b59b93ced6335f7de1a5a7
Antoine <ant.delalande@gmail.com>
2024-03-25 17:09:27 +0100

    added some style to my card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,26 @@
+const card = {
+    padding: "2em",
+    backgroundColor: "gray",
+    border: "2px solid black",
+}
+
+const cardImg = {
+    boxShadow: "0px 0px 5px gray",
+    backgroundColor: "white",
+    border: "2px solid black",
+}
+
+const cardText = {
+    backgroundColor: "white",
+    color: "black",
+    fontSize: "2em",
+    border: "2px solid black",
+}
+
 function PokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizarre" />
-        <figcaption>Bulbizarre</figcaption>
+    return <figure style={card}>
+        <img style={cardImg} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizarre" />
+        <figcaption style={cardText}>Bulbizarre</figcaption>
     </figure>
 }
 
