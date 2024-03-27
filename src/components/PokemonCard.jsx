b4fd54d26fa771257d01558059c45821a6b8829f
Antoine <ant.delalande@gmail.com>
2024-03-27 10:40:41 +0100

    changed general appearance

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,11 @@
 import PropTypes from "prop-types";
 
 const card = {
-    padding: "2em 2em 6em 2em",
-    height: "400px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    padding: "2em 2em 2em 2em",
+    height: "500px",
     width: "500px",
     backgroundColor: "#990e05",
     border: "2px solid black",
@@ -11,13 +14,24 @@ const card = {
 const cardImg = {
     boxShadow: "0px 0px 5px black",
     backgroundColor: "white",
-    height: "100%",
+    height: "90%",
     border: "2px solid black",
 }
 
 const cardText = {
     backgroundColor: "white",
     color: "black",
+    fontSize: "2em",
+    border: "2px solid black",
+}
+
+const noImg = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor: "white",
+    color: "black",
+    height: "100%",
     fontSize: "2em",
     border: "2px solid black",
 }
@@ -32,7 +46,7 @@ PokemonCard.propTypes = {
 
 function PokemonCard({pokemon}) {
     return <figure style={card}>
-        {pokemon.imgSrc ? <img style={cardImg} src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+        {pokemon.imgSrc ? <img style={cardImg} src={pokemon.imgSrc} alt={pokemon.name} /> : <p style={noImg}>???</p>}
         <figcaption style={cardText}>{pokemon.name}</figcaption>
     </figure>
 
